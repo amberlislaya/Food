d3f5fda49474aba51fe9e85985219b287fc2f15a
amberlislaya <laya.playapps@gmail.com>
2023-11-27 12:33:39 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+ content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      color: {
+        brightColor: "#F4511F",
+        backgroundColor: "#b7bca9",
+        lightText: "#959595",
+
+      },
+    },
+  },
+  plugins: [],
+}
+
